6def1eae1a2083823d288e07a49787c88620143a
matheusmor <matieumor@hotmail.com>
2020-07-28 15:49:41 -0300

    update exercicio matheus

---
+++ matheus/lista nivelamento/src/exercicios_java_serratec/variaveis_condicionais_lacos.java
@@ -0,0 +1,19 @@
+package exercicios_java_serratec;
+
+public class variaveis_condicionais_lacos {
+	public static void main(String[] args) {
+	funcoes_nivelamento exercicio= new funcoes_nivelamento();
+	
+	//questão 1
+	//exercicio.nomeCompleto();
+	//questão 2
+	//exercicio.temperatura();
+	//questão 3
+	//exercicio.somaImpar(5);
+	//questão 4
+	//exercicio.triangulo(4);
+	//questão 5
+	exercicio.palindromo();
+	}
+
+}
