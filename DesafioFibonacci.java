03ad3383e76a6c3ab18cbadf258d76957d71c227
RoResende <68281057+RoResende@users.noreply.github.com>
2020-07-30 09:28:38 -0300

    Exercícios

---
+++ DesafioFibonacci.java
@@ -0,0 +1,24 @@
+
+public class DesafioFibonacci {
+	
+	public static long fibonacci (int valor) {
+		
+		if(valor<2)
+			return 1;
+		else
+			return fibonacci(valor-1)+fibonacci(valor-2);
+	}
+
+	public static void main(String[] args) {
+		
+		int cont;
+		
+		for(cont=0;cont<=10;cont++) {
+			System.out.print(DesafioFibonacci.fibonacci(cont)+" , ");
+		}
+
+	}
+	
+	
+
+}
