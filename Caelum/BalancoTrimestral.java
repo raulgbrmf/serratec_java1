f381612a208cd3c4d3a28406ffea0f32eed73a1b
Rômulo Alves <romuloalves.rj@gmail.com>
2020-07-30 15:00:09 -0300

    Exercícios Caelum

---
+++ Caelum/BalancoTrimestral.java
@@ -0,0 +1,17 @@
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		int	gastosTrimestre	= gastosJaneiro + gastosFevereiro + gastosMarco;
+		int mediaMensal = gastosTrimestre / 3;
+		
+		System.out.println(gastosTrimestre);
+		System.out.println("Valor da média mensal = " + mediaMensal);
+
+	}
+
+}
