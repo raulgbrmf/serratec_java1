30c416844b954117ce903e04f0152ed2e1111257
nicholas014 <68038331+nicholas014@users.noreply.github.com>
2020-07-31 12:15:06 -0700

    GET_SET
    EXERCICIO

---
+++ exercicioGET&SET(5.8-)2.java
@@ -0,0 +1,21 @@
+class Conta {
+
+    private String titular;
+    private double saldo;
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+}
