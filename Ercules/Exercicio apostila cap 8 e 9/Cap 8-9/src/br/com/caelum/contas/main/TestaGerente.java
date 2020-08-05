050c183a28f4800707588617e0f3a232cb599e13
ErculesM <erculesm10@gmail.com>
2020-08-05 13:33:24 -0300

    Exercicio da apostila Caelum

---
+++ Ercules/Exercicio apostila cap 8 e 9/Cap 8-9/src/br/com/caelum/contas/main/TestaGerente.java
@@ -0,0 +1,14 @@
+package br.com.caelum.contas.main;
+
+import br.com.caelum.contas.funcionario.Gerente;
+
+public class TestaGerente {
+	public static void main(String[] args) {
+		
+		Gerente gerente = new Gerente();
+		gerente.setNome("João	da	Silva");
+		gerente.setSenha(4231);
+		gerente.setSalario(5000);
+		System.out.println(gerente.getBonificacao());
+	}
+}
