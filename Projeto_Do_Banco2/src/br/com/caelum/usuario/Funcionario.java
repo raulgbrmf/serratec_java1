eef00f1f571489dd4d0539348aa138e79d14d3f2
CiscottoM <68281406+CiscottoM@users.noreply.github.com>
2020-08-05 19:13:25 -0300

    Add files via upload

---
+++ Projeto_Do_Banco2/src/br/com/caelum/usuario/Funcionario.java
@@ -0,0 +1,46 @@
+package br.com.caelum.usuario;
+
+public abstract class Funcionario {
+	protected String nome ;
+	protected String cpf ;
+	protected double salario ;
+	
+	protected String nome() {
+		return this.nome;
+	}
+	protected String cpf() {
+		return this.cpf;
+	}
+	protected double salario() {
+		return this.salario;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public abstract double getBonificacao() ;
+		//return this.salario * 1.2;
+		
+}
+
+
+	
+
